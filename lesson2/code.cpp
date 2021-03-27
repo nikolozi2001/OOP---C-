19f2d14e20dfc3154108c9cdb2ccb0980d97e3c2
Nika Kachibaia <fsix@fsix.fsix>
2021-03-27 10:12:09 +0400

    added lecture2

---
+++ lesson2/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    string s1="Hello";
+    string s2 = s1;
+
+    cout<<"s1 --->" <<s1<<endl;
+}
