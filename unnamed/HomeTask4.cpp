d442752a52cdec669be0a86f6cafc973268cdb4d
nikolozi2001 <34250988+nikolozi2001@users.noreply.github.com>
2021-04-27 00:43:27 +0400

    added HomeWork (need to bug fix)

--- unnamed/HomeTask4.cpp
+++ unnamed/HomeTask4.cpp
@@ -3,10 +3,26 @@
 
 using namespace std;
 
-void swap (int a, int b){
-    
+
+int sum(int a, int b){
+	return (a + b);
+}
+
+int sub(int c, int a){
+    return (c - a);
 }
 
 int main(){
+    int a, b, c, s;
+
+	cout << "Enter 2 numbers - ";
+	cin >> a >> b;
+	
+	c = sum(a,b);
+	cout<<"sum is "<<c<<endl;
+
+    s = sub(c,a);
+    cout<<"sub is "<<s<<endl;
+    
 
 }
