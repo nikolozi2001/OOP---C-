ede38b6154053ab4af5f7abdd96ce66d51ee2292
nikolozi2001 <34250988+nikolozi2001@users.noreply.github.com>
2021-05-26 22:17:51 +0400

    trying

--- unnamed/HomeTask5.cpp
+++ unnamed/HomeTask5.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
- 
- using namespace std;
+using namespace std;
 
+int x;
+int Array[x+1];
+int n, first, last;
+
+void Input(){
+cin>>n>>first>>last;
+   for(int i=1; i<=n; i++) 
+            cin>>Array[i];
+}
+
+int Calc_Distance(){
+   int answer = 0;
+      for(int i=first; i<=last; i++){
+          answer += Array[i];
+             }
+          return answer;
+}
+
+int main(){
+      Input();
+    cout<<Calc_Distance()
+
+return 0;
+}
